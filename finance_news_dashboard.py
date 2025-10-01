ccbc59cd8f3adf8459a0aa9e955b5793f7bfcd92
kabirsrana193-dot <kabirsrana193@gmail.com>
2025-10-01 10:54:42 +0530

    Update finance_news_dashboard.py

--- finance_news_dashboard.py
+++ finance_news_dashboard.py
@@ -115,5 +115,5 @@ def update_news(n_clicks):
 # Run app
 # --------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
